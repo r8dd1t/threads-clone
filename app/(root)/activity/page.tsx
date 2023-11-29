728c58dff310640d1d1a31ee12605e824d3165b0
r8dd1t <r8ddit@gmail.com>
2023-11-29 14:54:38 +0200

    Activity page

--- app/(root)/activity/page.tsx
+++ app/(root)/activity/page.tsx
@@ -1,8 +1,55 @@
-import React from 'react'
+import { redirect } from "next/navigation";
+import { currentUser } from "@clerk/nextjs";
 
-const Page = () => {
+import UserCard from "@/components/cards/UserCard";
+import Searchbar from "@/components/shared/Searchbar";
+import Pagination from "@/components/shared/Pagination";
+
+import { fetchUser, getActivity } from "@/lib/actions/user.actions";
+import Link from "next/link";
+import Image from "next/image";
+
+async function Page() {
+
+  const user = await currentUser();
+  if (!user) return null;
+
+  const userInfo = await fetchUser(user.id);
+  if (!userInfo?.onboarded) redirect("/onboarding");
+
+  //getActivity
+  const activity = await getActivity(userInfo._id);
+
   return (
-    <h1 className='head-text mb-10'>Activity</h1>
+    <section>
+      <h1 className="head-text mb-10">Activity</h1>
+
+      <section className="mt-10 flex flex-col gap-5">
+        {activity.length > 0 ? (
+          <>
+            {activity.map((activity) => (
+              <Link key={activity._id} href={`/thread/${activity.parentId}`}>
+                <article className="activity-card">
+                  <Image
+                    src={activity.author.image}
+                    alt="Profile Picture"
+                    width={20}
+                    height={20}
+                    className="rounded-full object-cover"
+                  />
+                  <p className="!text-small-regular text-light-1">
+                    <span className="mr-1 text-primary-500">
+                      {activity.author.name}
+                    </span>{" "}
+                    replied to your thread
+                  </p>
+                </article>
+              </Link>
+            ))}
+          </>
+        ): <p className="!text-base-regular text-light-3">No activity yet</p>}
+      </section>
+    </section>
   )
 }
 
